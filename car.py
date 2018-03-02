7326012623cc4f90cca9bbfeb4636df7de45da62
Ukuthula <julian.hesse4@gmx.de>
2018-03-02 16:54:06 +0100

    reached a score of 22.276.525

--- car.py
+++ car.py
@@ -3,31 +3,85 @@ import math
 class Car:
 
     def __init__(self, aim_x, aim_y, numberID):
-        self.rideNumber = []
+        self.ridesNumberList = []
         self.pos_x = 0
         self.pos_y = 0
-        self.aimAt_x = 0
-        self.aimAt_y = 0
+        self.available = True
         self.id = numberID
+        self.currentTime = 0
 
 
-    def addRideNumber(number):
-        rideNumber = number
+    #### Functions for Car ####
+    def distanceCalc(self, point1, point2):
+        '''
+        Returns the distance between two points
+        '''
+        distance = abs(point1[0] - point2[0]) + abs(point1[1]- point2[1])
 
-    def getRidesNumberList():
+        return distance
+
+    def reachable(self, ride):
+        '''
+        Checks if a ride can be done in time.
+        '''
+        pathToStart = self.distanceCalc((self.pos_x, self.pos_y), ride[0])
+        pathToFinish = self.distanceCalc(ride[0], ride[1])
+
+        return self.currentTime + pathToStart + pathToFinish < ride[3]
+
+    def getIoNextRide(self, ridesList):
+        '''
+        Gets the index of the next possible ride:
+        '''
+        index = []
+        for count in range(len(ridesList)):
+            if self.reachable(ridesList[count]):
+                index = count
+                break
+
+        return index
+
+    def timePassed(self, ride):
+        '''
+        Adjusts the currentTime of the car to time needed for the ride
+        '''
+        pathToStart = self.distanceCalc((self.pos_x, self.pos_y), ride[0])
+        pathToFinish = self.distanceCalc(ride[0], ride[1])
+
+        self.currentTime += pathToStart + pathToFinish
+
+    def takeNextRide(self, ridesList):
+        '''
+        Takes the next possible ride in ridesList and sets sef.available false, when no ride is possible
+        '''
+        index = self.getIoNextRide(ridesList)
+        if index == []:
+            self.available = False
+            return ridesList
+        
+        self.timePassed(ridesList[index])
+        self.ridesNumberList.append(ridesList[index][4])
+        # self.ridesNumberList.sort()
+
+        # removes the taken ride from the list
+        ridesList.pop(index)
+
+        return ridesList
+
+
+    #### Getter and Setter for the class #####
+
+    def getRidesNumberList(self):
         return self.ridesNumberList
 
-    def getPosition_x():
+    def getPosition_x(self):
         return self.pos_x
     
-    def getPosition_y():
+    def getPosition_y(self):
         return self.pos_y
 
-    def getAim_y():
-        return self.aimAt_y
-
-    def getAim_x():
-        return self.aimAt_x
+    def available(self):
+        return self.available
     
-    def getID():
+    def getID(self):
         return self.id
--- main.py
+++ main.py
@@ -28,6 +28,16 @@ def readFile(filename):
 
         return rows, cols, vehicles, rides, bonus, steps, lists
 
+
+def writeFile(filename, _output):
+    '''
+    This method writes the input to a given file-location.
+    '''
+
+    with open(filename, 'w') as file:
+        file.write(_output)
+
+
 def distanceCalc(point1, point2):
     '''
     Returns the distance between two points
@@ -62,6 +72,26 @@ def sortListByDistanceTo00(rideList):
         rideList[position]=currentvalue
     return rideList
 
+def sortListByBeginTime(rideList):
+    ##
+    less = []
+    equal = []
+    greater = []
+
+    if len(rideList) > 1:
+        pivot = rideList[0][2]
+        for ride in rideList:
+            if ride[2] < pivot:
+                less.append(ride)
+            if ride[2] == pivot:
+                equal.append(ride)
+            if ride[2] > pivot:
+                greater.append(ride)
+        # Don't forget to return something!
+        return sortListByBeginTime(less)+equal+sortListByBeginTime(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to hande the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return rideList
 
 def timeToReachStart(ride):
     timeAvailable = ride[3] - ride [2] - distanceCalc(ride[0], ride[1]) - 1
@@ -82,18 +112,14 @@ def findClosestRide(rideList, pos_x, pos_y, currentTime):
     return currentClosest
 
 
-def assignNewRide(currentCar, rideList):
-    nextRide = findClosestRide(sortedDistanceList)
-    currentCar.aimAt_x = nextRide[]
-    currentCar.aimAt_y = sortedDistanceList[count][0][1]
-    currentCar.rideNumber = sortedDistanceList[count][4]
-
-    return currentCar
-
 ##########################################################
 
 ##### Read the File
-rows, cols, vehicles, rides, bonus, steps, ridesList = readFile(sys.argv[1])
+nameOfFile = sys.argv[1]
+inputFile = "data/" + nameOfFile
+outputFile = "output/" + nameOfFile
+
+rows, cols, vehicles, rides, bonus, steps, ridesList = readFile(inputFile)
 
 print('rows', rows)
 print('cols', cols)
@@ -112,11 +138,13 @@ purifiedList = purifyList(ridesList)
 print(distanceCalc(ridesList[0][0], ridesList[0][1]))
 
 ###### Sort purifiedList 
-sortedDistanceList = sortListByDistanceTo00(purifiedList)
+#sortedDistanceList = sortListByDistanceTo00(purifiedList)
+#print(sortedDistanceList)
+#print(sortedDistanceList[0][0][0])
 
-print(sortedDistanceList)
-
-print(sortedDistanceList[0][0][0])
+sortedTimeList = sortListByBeginTime(purifiedList)
+# print(sortedTimeList)
+print(sortedTimeList[0][0][0])
 
 ###### initialize car-objects
 carList = []
@@ -125,33 +153,28 @@ for id in range(vehicles):
     newCar = Car([],[],id)    
     carList.append(newCar)
 
+# print(carList)
+
+###### Assign the rides to each car
+
+for car in carList:
+    while car.available:
+        sortedTimeList = car.takeNextRide(sortedTimeList)
 
 
-print(carList)
 
-# send the cars to the destinations
-for count in range(vehicles):
-    carList[count].aimAt_x = sortedDistanceList[count][0][0]
-    carList[count].aimAt_y = sortedDistanceList[count][0][1]
-    carList[count].rideNumber = count
+###### Output the fullfilled rides
 
-for count in range(vehicles):
-    sortedDistanceList.pop(0)
+output = ""
 
+for count in range(len(carList)):
+    output += str(len(carList[count].ridesNumberList)) + ' '
 
-finishedRidesList = []
+    for number in carList[count].ridesNumberList:
+        output += str(number) + ' '
+    
+    output += '\n'
 
-for tick in range(steps):
-    # check if any car has nothing to do
-    for count in range(vehicles):
-        if (carList[count].aimAt_x == []):
-            nextRide = findClosestRide(sortedDistanceList)
-            carList[count].aimAt_x = sortedDistanceList[count][0][0]
-            carList[count].aimAt_y = sortedDistanceList[count][0][1]
-            carList[count].rideNumber = sortedDistanceList[count][4]
-        else:
-            if ( carList[count].pos_x == carList[count].aimAt_x ) and ( carList[count].pos_y == carList[count].aimAt_y ):
-                finishedRidesList.append((carList[count].rideNumber, carList[count].id))
-                carList[count].aimAt_x = []
-                carList[count].aimAt_y = []
-        
+#print(output)
+print(outputFile)
+writeFile(outputFile, output)
